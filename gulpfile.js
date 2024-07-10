06759b25d629e62aac7fad5655e9d465ebd35e46
Germán <germansalamancav@gmail.com>
2024-07-10 02:32:57 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass', function(){
+	return gulp.src('sass/*scss')
+		.pipe(sass())
+		.pipe(gulp.dest('assets'))
+})
+
+gulp.task('watch', function(){
+	gulp.watch('sass/**/*.scss', gulp.series('sass'))
+})
